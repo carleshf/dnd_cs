a5f9d955dae417c7392d8b3db233fccbfda15447
Carles Hernandez <2046180+carleshf@users.noreply.github.com>
2020-08-21 17:12:19 +0200

    Added the ability to create conditional acctions.

--- cs.js
+++ cs.js
@@ -66,12 +66,13 @@ class Character {
 		this.init = this.mods[1]
 		this.hp = hp
 		this.ca = ca
-		this.acctions = []
+		this.default = []
+		this.actions = []
 	}
 
 	show() {
 		// Prints the definition of a character
-		let x = '[CHARACTER] ' + this.name
+		let x = '[CHARACTER] ' + this.name + ' (' + (this.default.length + this.actions.length) + ' action(s))'
 		console.group()
 		console.log(x)
 		//console.log('-'.repeat(x.length))
@@ -81,6 +82,23 @@ class Character {
 		}
 		//console.log('-'.repeat(x.length))
 		console.groupEnd()
+	}
+
+	addAction(action) {
+		if(!action.valid) {
+			console.log('Given action is invalid.')
+		} else {
+			if(action.constructor.name == 'DefaultAction') {
+				if(this.default.length == 1) {
+					console.warn('Current character already has a default action. It will be overwritten.')
+				}
+				this.default = [action]
+			} else if(action.constructor.name == 'ConditionalAction') {
+				this.actions.push(action)
+			} else {
+				console.log('Invalid action. Only DefaultAction and ConditionalAction can be added.')
+			}
+		}
 	}
 }
 
@@ -171,11 +189,11 @@ class Action {
 
 	show() {
 		// Prints the definition of an action
-		var str = this.type + ' to '
+		var str = this.type + ' '
 		if(this.tar_con != '') {
-			str = str + this.tar_con + ' '
-		} else {
-			str = str + 'random '
+			str = str + 'to ' + this.tar_con + ' '
+		} else if(this.target != 'own') {
+			str = str + 'to random '
 		}
 		str = str + this.target + ' '
 		if(this.hab_dic != '') {
@@ -200,17 +218,103 @@ class Action {
 
 		str = str + ' points'
 
+		return(str)
+	}
+}
+
+class DefaultAction extends Action {
+	constructor(...args) {
+		// Allows to define a DefaultAction
+		// All characters should have a default action to be used when no
+		// other possibilities exists.
+		super(...args)
+		this.action_type = 'default'
+	}
+
+	show() {
+		// Prints the definition of an action
 		console.group()
-		console.log('[ACTION]')
-		console.log(str)
+		console.log('[DEFAULT ACTION]')
+		console.log(super.show())
 		console.groupEnd()
 	}
 }
 
+class ConditionalAction extends Action {
+	constructor(...args) {
+		// Allows to define a ConditionalAction
+		// Characters may have a series of conditional actions that represents
+		// the use of objects (healing potions) or spells which number of
+		// uses are limited and which performance may be conditional to a 
+		// specific situation.
+		super(...args)
+		this.action_type = 'conditional'
+		if(args.length != 9) {
+			console.error('Invalid call to ConditionalAction constructor. 9 arguments are required being the last one the number of charges.')
+			this.charges = 0
+		} else {
+			this.charges = args[8]
+		}
+		this.condition = []
+	}
+
+	show() {
+		// Prints the definition of an action
+		console.group()
+		console.log('[CONDITIONAL ACTION]')
+		console.log(super.show() + ' (number of charges: ' + this.charges + ')')
+		console.groupEnd()
+	}
+
+	addCondition(con_src, con_atr, con_chk) {
+		// Allows to specify a condition to indicate when this action has to be
+		// performed. If no condition is defined, it will be performed until 
+		// all charges are used.
+		//    . con_src (str): Defined the source of the condition.
+		//        enemy, own
+		//    . con_atr (str): Attribute to be checked.
+		//        hp, ac
+		//    . con_chk (str): Comparison to be done using to particles, the 
+		//      first part the equality/inequality and the second the value,
+		//      separated by semicolon. 
+		//        particle 1: <, <=, =, >, >=, !=
+		//        particle 2: (number), half, third
+		if(this.condition.length == 1) {
+			console.warn('Current action already has a condition. It will be overwritten.')
+		}
+
+		con_src = con_src.toLowerCase()
+		con_atr = con_atr.toLowerCase()
+		con_chk = con_chk.toLowerCase()
+
+		if(con_src != 'enemy' & con_src != 'own') {
+			this.valid = false
+			console.error('Created a conditional action with con_src "' + con_src + '", which can only take "enemy" or "own".')
+		}
+
+		if(con_atr != 'hp' & con_atr != 'ac') {
+			this.valid = false
+			console.error('Created a conditional action with con_atr "' + con_atr + '", which can only take "hp" or "ac".')
+		}
+
+		con_chk = con_chk.split(';')
+		if(con_chk[0] == '' | con_chk[1] == '' | con_chk[0] == undefined | con_chk[1] == undefined) {
+			this.valid = false
+			console.error('Created conditional action with invalid con_chk. No equality/inequality ("' + con_chk[0] + '") or value ("' + con_chk[1] + '") was given.')
+		}
+
+		this.condition = [con_src, con_atr, con_chk[0], con_chk[1]]
+	}
+}
 
 
 var mage2 = new Character('mage', [11, 12, 13, 11, 16, 15], 12, 11)
+var ac1 = new DefaultAction('attack', 'enemy', '', '', 1, 10, '1d4', 2)
+
+mage2.addAction(ac1)
 mage2.show()
 
-var ac1 = new Action('heal', 'enemy', '', '', 1, 10, '1d4', 2)
-ac1.show()
+
+var dc1 = new ConditionalAction('heal', 'own', '', '', 1, 10, '2d4', 2, 3)
+dc1.addCondition('own', 'hp', '=;half')
+dc1.show()
